b25b74253a7b319fae82d4f8258dbcad11482e6b
IshaniBid <132675098+IshaniBid@users.noreply.github.com>
2023-05-05 14:39:24 +0100

    Implemented Step 2 : Simple Offers
    Updated the code with 2 simple offers . This code is fully tested in Eclipse Scala IDE 
    Date - 05/05/2023

--- Scala_task.scala
+++ Scala_task.scala
@@ -29,13 +29,21 @@ def checkout(items: List[String]): String = {
   val Applecount = items.count(_ == "Apple")
   
   //Calculate the cost of apples
-  val appleCost = Applecount * APPLE_PRICE
+  //val appleCost = Applecount * APPLE_PRICE
   
+    //Step 2: Offer 1
+    //Calculate the cost of apples after the "buy one get one free" offer
+    val appleCost = (Applecount / 2 + Applecount % 2) * APPLE_PRICE
+    
   // Count the number of oranges in the list of items
   val OrangeCount = items.count(_ == "Orange")
   
-  // Calculate the cost of oranges
-  val orangeCost = OrangeCount * ORANGE_PRICE
+  //Calculate the cost of oranges
+  //val orangeCost = OrangeCount * ORANGE_PRICE
+    
+    //Step 2: Offer 2
+    // Calculate the cost of oranges after the "3 for the price of 2" offer or "Buy 2 get 1 free" offer
+    val orangeCost = (OrangeCount / 3 * 2 + OrangeCount % 3) * ORANGE_PRICE
   
   // Calculate the total cost of all items
   totalCost = appleCost + orangeCost
@@ -46,9 +54,16 @@ def checkout(items: List[String]): String = {
   
   // Return the total cost as a string in pounds and pence format
   s"Total cost: $pounds.${pence} pounds"
+}
   
   
-  
-  // Example
-  val items = List("Apple", "Apple" ,"Orange", "Apple", "Apple")
-println(checkout(items)) //Total cost: 2.65 pounds
+  // Example for Step1:
+  //val items = List("Apple", "Apple" ,"Orange", "Apple", "Apple")
+    //println(checkout(items)) //Total cost: 2.65 pounds
+    
+    //Example for Step2:
+val items = List( "Orange", "Orange", "Apple", "Apple", "Orange", "Apple", "Orange")
+    println(checkout(items)) //Total cost: 1.95 pounds
+        
+    }
+}
